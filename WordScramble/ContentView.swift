5ae373ed62f63deefe1ce3b35e000dc8f8ee6500
Roberto <roberto.tiboldi@ulbisbiu.ro>
2022-04-14 13:21:28 +0300

    Add textfield and display the words list

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -1,8 +1,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
     var body: some View {
-        Text("Hello World!")
+        NavigationView{
+            List{
+                Section{
+                    TextField("Enter your word", text: $newWord)
+                        .autocapitalization(.none)
+                }
+                Section{
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack{
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
+                }
+            }
+        }
+        .navigationTitle(rootWord)
+        .onSubmit(addNewWord) //on submit need a func that have no parameters and return no value like our addNewWord
+    }
+    
+    //func that have no parameters and return no value
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        guard answer.count > 0 else { return }
+        
+        withAnimation {
+            usedWords.insert(answer, at: 0)
+        }
+        newWord = ""
+        
     }
 }
 
