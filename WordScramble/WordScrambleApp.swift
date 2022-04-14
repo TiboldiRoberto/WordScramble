59893fde40b2580aad6fd8010fd57a8c0dbfa96f
Roberto <roberto.tiboldi@ulbisbiu.ro>
2022-04-14 11:53:28 +0300

    Starter Project

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Roberto on 14.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
