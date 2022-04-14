1fe19a915c16e420ab0aea7a300de1ee69121c83
Roberto <roberto.tiboldi@ulbisbiu.ro>
2022-04-14 14:39:40 +0300

    Add the validator

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -4,6 +4,11 @@ struct ContentView: View {
     @State private var usedWords = [String]()
     @State private var rootWord = ""
     @State private var newWord = ""
+    
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    
     var body: some View {
         NavigationView{
             List{
@@ -23,6 +28,11 @@ struct ContentView: View {
             .navigationTitle(rootWord)
             .onSubmit(addNewWord) //on submit need a func that have no parameters and return no value like our addNewWord
             .onAppear(perform: startGame)
+            .alert(errorTitle, isPresented: $showingError) {
+                Button("OK", role: .cancel) {}
+            } message: {
+                Text(errorMessage)
+            }
         }
     }
     
@@ -30,6 +40,21 @@ struct ContentView: View {
     func addNewWord() {
         let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
         guard answer.count > 0 else { return }
+        
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word not possible", message: "Be more original!")
+            return
+        }
+            
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'!")
+            return
+        }
+        
+        guard isReal(word: answer)else {
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            return
+        }
         
         withAnimation {
             usedWords.insert(answer, at: 0)
@@ -46,8 +71,38 @@ struct ContentView: View {
                 return
             }
         }
-        
         fatalError("Could not load start.txt file")
+    }
+    
+    func isOriginal (word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    func isPossible (word: String) -> Bool {
+        var tempWord = rootWord
+        
+        for letter in word{
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func isReal(word: String)-> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func wordError(title: String, message: String){
+        errorTitle = title
+        errorMessage = message
+        showingError = true
     }
 }
 
